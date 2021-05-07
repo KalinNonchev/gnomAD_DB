976a4234ca40e7eb8598d5c7c87ffea27a1e4a75
Kalin Nonchev <nonchev@ouga04.cmm.in.tum.de>
2021-05-07 12:47:09 +0200

    log

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gnomad_db',
-      version='0.0.2',
+      version='0.0.0',
       description='Query gnomAD MAF',
       author='KalinNonchev',
       author_email='boo@foo.com',
