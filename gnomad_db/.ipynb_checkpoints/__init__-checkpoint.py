0e66b492706f9483c82af4041776a886643794dc
Kalin Nonchev <nonchev@ouga04.cmm.in.tum.de>
2021-05-07 13:16:28 +0200

    add gitignore

--- gnomad_db/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,1 +0,0 @@
-import gnomad_db
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='gnomad_db',
-      version='0.0.0',
+      version='0.0.1',
       description='Query gnomAD MAF',
       author='KalinNonchev',
       author_email='boo@foo.com',
       packages=find_packages(),  # find packages
       include_package_data=True,
-      install_requires=['pandas', 'numpy', 'pysqlite3']  # external packages as dependencies
+      install_requires=['pandas', 'numpy']  # external packages as dependencies
       )
