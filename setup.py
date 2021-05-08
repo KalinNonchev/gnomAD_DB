f6264ea4c08a99e691730d3bdf75a951ba0f31b0
Kalin Nonchev <50597791+KalinNonchev@users.noreply.github.com>
2021-05-08 20:15:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='gnomad_db',
       version='0.0.1',
-      description='Query gnomAD MAF',
+      description='This package scales the huge gnomAD files to a SQLite database, which is easy and fast to query. It extracts from a gnomAD vcf the minor allele frequency for each variants.',
       author='KalinNonchev',
       author_email='boo@foo.com',
       packages=find_packages(),  # find packages
