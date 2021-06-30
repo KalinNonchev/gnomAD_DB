2b72a80f83bbef4764c252811644304f5d487631
Kalin Nonchev <nonchev@ouga06.cmm.in.tum.de>
2021-06-30 18:02:53 +0200

    rename

--- gnomad_db/utils.py
+++ gnomad_db/utils.py
@@ -13,25 +13,26 @@ class DownloadProgressBar(tqdm):
         self.update(b * bsize - self.n)
 
 
-def download_url(url, output_path):
+def download_url(url, output_dir):
     with DownloadProgressBar(unit='B', unit_scale=True,
                              miniters=1, desc=url.split('/')[-1]) as t:
-        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+        urllib.request.urlretrieve(url, filename=f"{output_dir}/gnomad_db.sqlite3.gz", reporthook=t.update_to)
         time.sleep(5)
 
-def unzip(path_to_zip_file):
-    file_name = path_to_zip_file.replace(".gz", "")
-    with gzip.open(path_to_zip_file, 'rb') as f_in:
-        with open(file_name, 'wb') as f_out:
+def unzip(output_dir):
+    file_name_in = f"{output_dir}/gnomad_db.sqlite3.gz"
+    file_name_out = f"{output_dir}/gnomad_db.sqlite3"
+    with gzip.open(file_name_in, 'rb') as f_in:
+        with open(file_name_out, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
     time.sleep(5)
     os.remove(path_to_zip_file)
     print(f"Database location: {file_name}")
 
-def download_and_unzip_file(url, output_path):
+def download_and_unzip_file(url, output_dir):
     print("Starting downloading...")
-    download_url(url, output_path)
+    download_url(url, output_dir)
     print("Starting unzipping. This can take some time...")
-    unzip(output_path)
+    unzip(output_dir)
     print("Done!")
     
